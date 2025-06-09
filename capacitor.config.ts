b57bd4c604a9a0167d2efd6fcee9de1138819a68
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-06-09 10:23:56 +0200

    maj

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'fr.silvercore.silvernote',
   appName: 'silvernote',
-  webDir: 'dist'
+  webDir: 'dist',
+  android: {
+    icon: 'resources/icon.png',
+  },
+  ios: {
+    icon: 'resources/icon.png',
+  },
 };
 
 export default config;
