805e52fa2925c88b49db9ce87300760b5c6d7fd6
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-06-10 17:12:12 +0200

    maj

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
-import fs from 'fs';
+import * as fs from 'fs';
 import path from 'path';
 import fetch from 'node-fetch';
 import unzipper from 'unzipper';
+import { exec } from 'child_process';
+import { promisify } from 'util';
 
-const front_path: string = path.join('dist', 'app', 'dist');
+const front_path: string = path.join('dist', 'app', 'public');
 const back_path: string = path.join('dist', 'app');
 const temp_dir: string = path.join('dist', 'temp');
+
+const execAsync = promisify(exec);
+
+async function command(command: string): Promise<string> {
+
+    try {
+
+        const { stdout, stderr } = await execAsync(command, {
+            cwd: path.join('dist', 'app'),
+            encoding: 'utf-8'
+        });
+        
+        if (stderr) {
+            console.warn('Avertissement:', stderr);
+        }
+        
+        return stdout.trim();
+
+    } catch (error) {
+        console.error('Erreur lors de l\'exécution:', error);
+        throw error;
+    }
+
+}
 
 async function download(url: string, name: string): Promise<void> {
 
@@ -38,6 +64,13 @@ async function unzip(path: string, outpath: string) {
 
 }
 
+function launch_app() {
+    command("npm i");
+    //command("npm stop");
+    command("npm start");
+}
+
+
 async function app(): Promise<void> {
 
     console.log("Démarage du serveur...");
@@ -61,11 +94,13 @@ async function app(): Promise<void> {
     const latest_realise: any = await fetch('https://api.github.com/repos/silvercore-git/silvernote/releases/latest').then(res => res.json());
 
     const tag_name: string = latest_realise.tag_name;
-    const base = 'https://github.com/SilverCore-Git/silvernote/releases/download/';
-    const tag = tag_name;
-    const version = tag.slice(1);
-    const url = new URL(`${tag}/silvernote-front-${version}.zip`, base).href;
-    const zipName = `silvernote-front-${version}.zip`;
+    const base: string = 'https://github.com/SilverCore-Git/silvernote/releases/download/';
+    const tag: string = tag_name;
+    const version: string = tag.slice(1);
+    const front_url: string = new URL(`${tag}/silvernote-front-${version}.zip`, base).href;
+    const front_zipName: string = `silvernote-front-${version}.zip`;
+    const back_url: string = new URL(`${tag}/silvernote-back-${version}.zip`, base).href;
+    const back_zipName: string = `silvernote-back-${version}.zip`;
 
     if (fs.existsSync(path.join(back_path, 'package.json'))) {
 
@@ -73,12 +108,66 @@ async function app(): Promise<void> {
             assert: { type: "json" }
         });
 
-        if (`v${version}` !== latest_realise.tag_name) {
+        if (`v${version}` !== tag_name) {
             
+            await download(back_url, back_zipName);
+
+            await unzip(path.join(temp_dir, back_zipName), path.join(temp_dir, `silvernote-back-${version}`));
+
+            try {
+
+                const items: string[] = await fs.promises.readdir(path.join(temp_dir, `silvernote-back-${version}/dist`));
+
+                for (const item of items) {
+
+                    const sourcePath = path.join(path.join(temp_dir, `silvernote-back-${version}/dist`), item);
+                    const destPath = path.join(back_path, item);
+                    
+                    await fs.promises.cp(sourcePath, destPath, { recursive: true });
+                    
+                    await fs.promises.rm(sourcePath, { recursive: true, force: true });
+
+                }
+
+            }
+            catch (err) {
+                throw new Error(`Erreur lors du déplacement du dossier : ${err}`);
+            }
 
         }
-        
+
     }
+    else 
+    {
+
+        await download(back_url, back_zipName);
+
+        await unzip(path.join(temp_dir, back_zipName), path.join(temp_dir, `silvernote-back-${version}`));
+
+        try {
+
+            const items: string[] = await fs.promises.readdir(path.join(temp_dir, `silvernote-back-${version}/dist`));
+
+            for (const item of items) {
+
+                const sourcePath = path.join(path.join(temp_dir, `silvernote-back-${version}/dist`), item);
+                const destPath = path.join(back_path, item);
+                    
+                await fs.promises.cp(sourcePath, destPath, { recursive: true });
+                    
+                await fs.promises.rm(sourcePath, { recursive: true, force: true });
+
+            }
+
+        }
+        catch (err) {
+            throw new Error(`Erreur lors du déplacement du dossier : ${err}`);
+        }
+
+    }
+
+
+
 
     if (fs.existsSync(path.join(front_path, 'package.json'))) {
 
@@ -88,9 +177,29 @@ async function app(): Promise<void> {
 
         if (`v${version}` !== tag_name) {
             
-            await download(url, zipName);
+            await download(front_url, front_zipName);
 
-            await unzip(path.join(temp_dir, zipName), front_path);
+            await unzip(path.join(temp_dir, front_zipName),  path.join(temp_dir, `silvernote-front-${version}`));
+
+            try {
+
+                const items: string[] = await fs.promises.readdir(path.join(temp_dir, `silvernote-front-${version}/dist`));
+
+                for (const item of items) {
+
+                    const sourcePath = path.join(path.join(temp_dir, `silvernote-front-${version}/dist`), item);
+                    const destPath = path.join(front_path, item);
+                    
+                    await fs.promises.cp(sourcePath, destPath, { recursive: true });
+                    
+                    await fs.promises.rm(sourcePath, { recursive: true, force: true });
+
+                }
+
+            }
+            catch (err) {
+                throw new Error(`Erreur lors du déplacement du dossier : ${err}`);
+            }
 
         }
 
@@ -98,9 +207,29 @@ async function app(): Promise<void> {
     else 
     {
 
-        await download(url, zipName);
+        await download(front_url, front_zipName);
 
-        await unzip(path.join(temp_dir, zipName), front_path);
+        await unzip(path.join(temp_dir, front_zipName), path.join(temp_dir, `silvernote-front-${version}`));
+
+        try {
+
+            const items: string[] = await fs.promises.readdir(path.join(temp_dir, `silvernote-front-${version}/dist`));
+
+            for (const item of items) {
+
+                const sourcePath = path.join(path.join(temp_dir, `silvernote-front-${version}/dist`), item);
+                const destPath = path.join(front_path, item);
+                    
+                await fs.promises.cp(sourcePath, destPath, { recursive: true });
+                    
+                await fs.promises.rm(sourcePath, { recursive: true, force: true });
+
+            }
+
+        }
+        catch (err) {
+            throw new Error(`Erreur lors du déplacement du dossier : ${err}`);
+        }
 
     }
 
@@ -110,3 +239,5 @@ async function app(): Promise<void> {
 app().catch((err) => {
     console.error("Error on runing script :", err);
 })
+
+launch_app();
