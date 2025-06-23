991083e7f069523a0a1220a0bf68b1297bec1e30
mister_papaye <philippeetchtebaise@hotmail.com>
2025-06-23 14:23:17 +0200

    maj

--- front/server.js
+++ front/server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const path = require('path');
-const morgan = require('morgane');
 
 const app = express();
 
-app.use(morgan());
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.listen(8080, () => {
