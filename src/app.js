fd1061f47c4f207cc5c9641cc3fc274c90d56aca
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-06-25 16:29:15 +0200

    add server

---
+++ src/app.js
@@ -0,0 +1,41 @@
+
+const express = require('express');
+const morgan = require('morgan');
+const { auth } = require('express-openid-connect');
+require('dotenv').config();
+
+
+const app = express();
+const PORT = 3033;
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH_SECRET,
+  baseURL: 'http://localhost:3033',
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+
+app.use(auth(config));
+app.use(express.json());
+app.use(morgan('dev'));
+
+const router = require('./routes');
+app.use('/', router);
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Route non trouvée' });
+});
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Erreur serveur' });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Serveur en écoute sur http://localhost:${PORT}`);
+});
