377ee36a5aaf10467fc32bc683906885e45f112d
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-07-03 12:10:55 +0200

    add divers truc

--- src/app.js
+++ src/app.js
@@ -1,40 +1,32 @@
-
 const express = require('express');
 const morgan = require('morgan');
-const { auth } = require('express-openid-connect');
-const proxy = require('express-http-proxy');
 require('dotenv').config();
-
+const { clerkMiddleware, clerkClient, requireAuth, getAuth } = require('@clerk/express');
 
 const app = express();
 const PORT = 3033;
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.AUTH_SECRET,
-  baseURL: 'https://auth.silvernote.fr',
-  clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.env.ISSUER_BASE_URL
-};
-
-
-app.use(auth(config));
+app.use(clerkMiddleware())
 app.use(express.json());
 app.use(morgan('dev'));
 
+app.get('/protected', requireAuth(), async (req, res) => {
+  // Use `getAuth()` to get the user's `userId`
+  const { userId } = getAuth(req)
+
+  // Use Clerk's JavaScript Backend SDK to get the user's User object
+  const user = await clerkClient.users.getUser(userId)
+
+  return res.json({ user })
+})
+
 const router = require('./routes');
 app.use('/', router);
-
-app.use('/auth0', proxy('dev-ojbmzr24ly4ic8z2.us.auth0.com', {
-  proxyReqPathResolver: req => req.originalUrl.replace('/auth0', '')
-}));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Erreur serveur' });
 });
-
 
 app.listen(PORT, () => {
   console.log(`Serveur en écoute sur http://localhost:${PORT}`);
